df19f5f0251c56831ebb5399724c8f345a0da1be
Zigfried Hampel-Arias <zhampel@icecube.wisc.edu>
2018-06-11 15:17:05 -0500

    Adding image rotation function when loading data

--- load_data.py
+++ load_data.py
@@ -1,15 +1,22 @@
 from __future__ import print_function
 import numpy as np
+from scipy import ndimage
 
-def load_data(path_to_data):
+# Rotate image by angle given in degrees
+def rotate_image(image_to_rotate, rot_ang_deg)
+    rotated = ndimage.rotate(image_to_rotate, rot_ang_deg)
+    return rotated
+
+
+def load_data(path_to_data, rot_ang_deg=0.):
 
     # Load MNIST data set
     if (path_to_data == 'mnist'):
-        x_train, y_train, x_test, y_test = load_mnist()
+        x_train, y_train, x_test, y_test = load_mnist(rot_ang_deg)
         
-    # Load MNIST data set
-    if (path_to_data == 'mnist_rot'):
-        x_train, y_train, x_test, y_test = load_mnist_rot()
+    ## Load MNIST data set
+    #if (path_to_data == 'mnist_rot'):
+    #    x_train, y_train, x_test, y_test = load_mnist_rot()
 
     # convert the data to the right type
     x_train = x_train.astype('float32')
@@ -24,7 +31,7 @@ def load_data(path_to_data):
     return x_train, y_train, x_test, y_test
 
 # Specifically load hand-written digit set
-def load_mnist():
+def load_mnist(rot_ang_deg=0.):
     
     import keras
     from keras.datasets import mnist
@@ -40,6 +47,10 @@ def load_mnist():
     # because the MNIST is greyscale, we only have a single channel - RGB colour images would have 3
     x_train = x_train.reshape(n_samples, img_x, img_y, 1)
     x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 1)
+
+    if (rot_ang_deg != 0.):
+        x_train = rotate_image(x_train, rot_ang_deg)
+        x_test = rotate_image(x_test, rot_ang_deg)
     
     # convert class vectors to binary class matrices - this is for use in the
     # categorical_crossentropy loss below
@@ -81,15 +92,5 @@ def load_mnist_rot():
     # categorical_crossentropy loss below
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
-    #print(type(x_train))
-    #for j in range(n_samples):
-    #    x = x_train[j,:,:,0].copy()
-    #    x = np.rot90(x)
-    #    x_train[j,:,:,0] = x.copy()
-        
-    #for j in range(x_test.shape[0]):
-    #    x = x_test[j,:,:,0].copy()
-    #    x = np.rot90(x)
-    #    x_test[j,:,:,0] = x.copy()
     
     return x_train, y_train, x_test, y_test
