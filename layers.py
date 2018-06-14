4ef989cb6fdb6542501d6a83278ff31eb752cdc8
Zigfried Hampel-Arias <zhampel@icecube.wisc.edu>
2018-06-14 11:15:51 -0500

    Working rotation layer! Starting to include documentation

--- layers.py
+++ layers.py
@@ -3,7 +3,12 @@ from keras import backend as K
 import tensorflow as tf
 
 """
-Code inspired by 
+TensorFlow convolution layer incorporating
+rotations of the kernel, passing along the 
+maximum convolution product from the set of 
+rotations as the output activation.
+
+Inspired by 
 https://github.com/raghakot/deep-learning-experiments/
 """
 
@@ -11,57 +16,60 @@ permutation = [[1, 0], [0, 0], [0, 1], [2, 0], [1, 1], [0, 2], [2, 1], [2, 2], [
 
 
 def shift_rotate(w, shift=1):
+    """
+    Rotate kernel according to 
+    requested (via shift) permutation.
+
+    Parameters
+    ----------
+    w      : array_like
+             Input kernel
+    shift  : int
+             Requested permutation
+
+    Returns
+    -------
+    output : float
+             Maximum activation
+    """
     shape = w.get_shape()
     for i in range(shift):
         w = tf.reshape(tf.gather_nd(w, permutation), shape)
     return w
 
-
+# Convolution layer with rotated filter activations
 class Convolution2D_4(Convolution2D):
+    """
+       Convolution2D_4 inherits from Convolution2D
+       No new input variables, same output
+    """
     def call(self, x, mask=None):
+        # Grab the kernel(s)
         w = self.kernel
+        # Make list of rotated version
         w_rot = [w]
         for i in range(3):
             w = shift_rotate(w, shift=2)
             w_rot.append(w)
 
+        # List of activations for each rotation
         outputs = tf.stack([K.conv2d(x, w_i, strides=self.strides,
                                      padding=self.padding,
                                      data_format=self.data_format) for w_i in w_rot])
 
+        # Choose maximal activation to pass along
         output = K.max(outputs, 0)
 
+        # If a bias term, incorporate according to TensorFlow or Theano ordering
         if self.bias:
-            if self.data_format == 'th':
-                output += K.reshape(self.b, (1, self.nb_filter, 1, 1))
-            elif self.data_format == 'tf':
-                output += K.reshape(self.b, (1, 1, 1, self.nb_filter))
+            if self.data_format == 'channels_first':
+                output += K.reshape(self.bias, (1, self.filters, 1, 1))
+            elif self.data_format == 'channels_last':
+                output += K.reshape(self.bias, (1, 1, 1, self.filters))
             else:
                 raise ValueError('Invalid data_format:', self.data_format)
+
         output = self.activation(output)
+
         return output
 
-
-class Convolution2D_8(Convolution2D):
-    def call(self, x, mask=None):
-        w = self.kernel
-        w_rot = [w]
-        for i in range(7):
-            w = shift_rotate(w)
-            w_rot.append(w)
-
-        outputs = tf.stack([K.conv2d(x, w_i, strides=self.strides,
-                                     padding=self.padding,
-                                     data_format=self.data_format) for w_i in w_rot])
-
-        output = K.max(outputs, 0)
-
-        if self.bias:
-            if self.data_format == 'th':
-                output += K.reshape(self.b, (1, self.nb_filter, 1, 1))
-            elif self.data_format == 'tf':
-                output += K.reshape(self.b, (1, 1, 1, self.nb_filter))
-            else:
-                raise ValueError('Invalid data_format:', self.data_format)
-        output = self.activation(output)
-        return output
