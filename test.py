086e1f25509e0993a76f0812f6c9a2cfb438cdd2
Zigfried Hampel-Arias <zhampel@icecube.wisc.edu>
2018-06-18 22:49:34 -0500

    correcting confusion matrix and class report outputs

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ try:
 
     from load_data import test_img_generator
     from keras.models import model_from_json
-    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import classification_report, confusion_matrix
     from dir_utils import DataDirStruct, ModelDirStruct
 
 except ImportError as e:
@@ -42,7 +42,8 @@ def main():
     model_dir_struct = ModelDirStruct(modelpath)
    
     # Get requested sample size
-    num_samples = args.num_samples 
+    num_samples = args.num_samples
+
     # Testing generator
     test_gen = test_img_generator(dir_struct=data_dir_struct, \
                                   batch_size=num_samples)
@@ -58,7 +59,7 @@ def main():
     trained_model_json = json_file.read()
     json_file.close()
     trained_model = model_from_json(trained_model_json)
-    # load weights into model
+    # Load weights into model
     trained_model.load_weights(model_dir_struct.weights_file)
     print("Loaded model from disk")
 
@@ -66,14 +67,20 @@ def main():
     trained_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     scores = trained_model.evaluate_generator(test_gen, max_queue_size=num_samples, steps=10)
     print("Test %s: %.2f%%" % (trained_model.metrics_names[1], scores[1]*100))
-    # Running prediction for confusion matrix
-    y_predict = trained_model.predict_generator(test_gen, test_gen.n // num_samples + 1)
-    #y_predict = trained_model.predict_generator(test_gen, 1) #num_samples // batch_size + 1)
-    #Y_pred = model.predict_generator(validation_generator, num_of_test_samples // batch_size+1)
-    print(test_gen.classes)
-    print(y_predict)
-    cm = confusion_matrix(test_gen.class_indices, y_predict)
-    #cm = confusion_matrix(test_gen.classes, y_predict)
+    
+    # Running prediction
+    Y_pred = trained_model.predict_generator(test_gen, test_gen.n // num_samples + 1)
+    y_predict = np.argmax(Y_pred, axis=1)
+
+    # Confusion matrix
+    print('Confusion Matrix')
+    cm = confusion_matrix(test_gen.classes[0:len(y_predict)], y_predict)
+    print(cm)
+
+    # Classification report
+    print('Classification Report')
+    target_names = ['Cats', 'Dogs', 'Horse', 'poop', 't','a','b','c','x','n']
+    print(classification_report(test_gen.classes[0:len(y_predict)], y_predict, target_names=target_names))
 
 if __name__ == "__main__":
     main()
